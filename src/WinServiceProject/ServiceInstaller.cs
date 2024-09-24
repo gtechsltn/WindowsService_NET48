2530acf54e3a5a3cfe99eb2fa025cd4344a0fa65
Nguyen Viet Manh <manh.nguyen@thirdsight.net>
2024-09-24 17:24:43 +0800

    Init source code

---
+++ src/WinServiceProject/ServiceInstaller.cs
@@ -0,0 +1,53 @@
+﻿namespace WinServiceProject
+{
+    /// <summary>
+    /// Summary description for ProjectInstaller.
+    /// </summary>
+    [System.ComponentModel.RunInstaller(true)]
+    public class ProjectInstaller : System.Configuration.Install.Installer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        //private System.ComponentModel.Container components;
+        private System.ServiceProcess.ServiceInstaller serviceInstaller;
+
+        private System.ServiceProcess.ServiceProcessInstaller serviceProcessInstaller;
+
+        /// <summary>
+        /// This call is required by the Designer.
+        /// </summary>
+        public ProjectInstaller()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.serviceInstaller = new System.ServiceProcess.ServiceInstaller();
+            this.serviceProcessInstaller = new System.ServiceProcess.ServiceProcessInstaller();
+            //
+            // serviceInstaller
+            //
+            this.serviceInstaller.Description = "WinServiceDemo";
+            this.serviceInstaller.DisplayName = "WinServiceDemo";
+            this.serviceInstaller.ServiceName = "WinServiceDemo";
+            //
+            // serviceProcessInstaller
+            //
+            this.serviceProcessInstaller.Account = System.ServiceProcess.ServiceAccount.LocalService;
+            this.serviceProcessInstaller.Password = null;
+            this.serviceProcessInstaller.Username = null;
+            //
+            // ServiceInstaller
+            //
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.serviceProcessInstaller,
+            this.serviceInstaller});
+        }
+    }
+}
---
+++ src/WinServiceProject/WinService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.ServiceProcess;
+
+namespace WinServiceProject
+{
+    internal class WinService : ServiceBase
+    {
+        /// <summary>
+        /// The main entry point for the process
+        /// </summary>
+        private static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[] { new WinService() };
+            ServiceBase.Run(ServicesToRun);
+        }
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ServiceName = "WinServiceDemo";
+        }
+
+        private string folderPath = @"c:\temp";
+
+        /// <summary>
+        /// Set things in motion so your service can do its work.
+        /// </summary>
+        protected override void OnStart(string[] args)
+        {
+            if (!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+            }
+
+            FileStream fs = new FileStream(folderPath + "\\WinServiceDemo.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter m_streamWriter = new StreamWriter(fs);
+            m_streamWriter.BaseStream.Seek(0, SeekOrigin.End);
+            m_streamWriter.WriteLine("WinServiceDemo: Service Started at " + DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToShortTimeString() + "\n");
+            m_streamWriter.Flush();
+            m_streamWriter.Close();
+        }
+
+        /// <summary>
+        /// Stop this service.
+        /// </summary>
+        protected override void OnStop()
+        {
+            FileStream fs = new FileStream(folderPath + "\\WinServiceDemo.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter m_streamWriter = new StreamWriter(fs);
+            m_streamWriter.BaseStream.Seek(0, SeekOrigin.End);
+            m_streamWriter.WriteLine("WinServiceDemo: Service Stopped at " + DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToShortTimeString() + "\n");
+            m_streamWriter.Flush();
+            m_streamWriter.Close();
+        }
+    }
+}
